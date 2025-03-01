16bbe747a02fe3fec57173912616a7d60f0f5d1e
Abdulazizjoon <xakimovxadyatillo92@gmail.com>
2025-03-01 15:43:12 +0500

    .

--- compiled/index.js
+++ compiled/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// Foydalanuvchi klassi
 class Users {
     constructor(name, age, id) {
         this.name = name;
@@ -27,25 +26,20 @@ class Project {
         this.admin = admin;
     }
     getProjectInfo() {
-        return `Proyekt: ${this.projectName}, Sahifalar: ${this.pages}, Foydalanuvchilar: ${this.usersCount}, Admin: ${this.admin.name}`;
+        return `proyekt: ${this.projectName}, sahifalar: ${this.pages}, foydalanuvchilar: ${this.usersCount}, admin: ${this.admin.name}`;
     }
 }
 class SuperAdmin extends Admin {
-    constructor(name, age, id, role, permissions) {
+    constructor(name, age, id, role) {
         super(name, age, id, role);
-        this.permissions = permissions;
     }
     getPermissions() {
-        return `SuperAdmin ${this.name} quyidagi ruxsatlarga ega: ${this.permissions.join(", ")}`;
+        return `SuperAdmin ${this.name} quyidagi ruxsatlarga ega: qoshish, ochirish, ozgartirish`;
     }
 }
 const user = new Users("Abdulaziz", 16, 1);
 const admin = new Admin("Ali", 25, 2, "Admin");
-const superAdmin = new SuperAdmin("Hasan", 30, 3, "SuperAdmin", [
-    "Add Users",
-    "Delete Users",
-    "Edit Projects",
-]);
+const superAdmin = new SuperAdmin("Hasan", 30, 3, "SuperAdmin");
 const project = new Project("Kino Sayti", 10, 5000, admin);
 console.log(user.getUser());
 console.log(admin.getAdminInfo());
--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-// Foydalanuvchi klassi
 class Users {
   constructor(public name: string, public age: number, public id: number) {}
 
@@ -26,41 +25,27 @@ class Project {
   ) {}
 
   getProjectInfo(): string {
-    return `Proyekt: ${this.projectName}, Sahifalar: ${this.pages}, Foydalanuvchilar: ${this.usersCount}, Admin: ${this.admin.name}`;
+    return `proyekt: ${this.projectName}, sahifalar: ${this.pages}, foydalanuvchilar: ${this.usersCount}, admin: ${this.admin.name}`;
   }
 }
 
 class SuperAdmin extends Admin {
-  constructor(
-    name: string,
-    age: number,
-    id: number,
-    role: string,
-    public permissions: string[]
-  ) {
+  constructor(name: string, age: number, id: number, role: string) {
     super(name, age, id, role);
   }
 
   getPermissions(): string {
-    return `SuperAdmin ${
-      this.name
-    } quyidagi ruxsatlarga ega: ${this.permissions.join(", ")}`;
+    return `SuperAdmin ${this.name} quyidagi ruxsatlarga ega: qoshish, ochirish, ozgartirish`;
   }
 }
 
 const user = new Users("Abdulaziz", 16, 1);
 const admin = new Admin("Ali", 25, 2, "Admin");
-const superAdmin = new SuperAdmin("Hasan", 30, 3, "SuperAdmin", [
-  "Add Users",
-  "Delete Users",
-  "Edit Projects",
-]);
+const superAdmin = new SuperAdmin("Hasan", 30, 3, "SuperAdmin");
+
 const project = new Project("Kino Sayti", 10, 5000, admin);
 
 console.log(user.getUser());
-
 console.log(admin.getAdminInfo());
-
 console.log(superAdmin.getPermissions());
-
 console.log(project.getProjectInfo());
